723c5fe89cbd8ca2ffade42286b95a002d376fae
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-12-04 16:37:35 -0600

    Initial Commit

---
+++ Dmytro-Ryshchuk-AnswersAi-iOS/Dmytro_Ryshchuk_AnswersAi_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dmytro_Ryshchuk_AnswersAi_iOSApp.swift
+//  Dmytro-Ryshchuk-AnswersAi-iOS
+//
+//  Created by Dmytro Ryshchuk on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Dmytro_Ryshchuk_AnswersAi_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
